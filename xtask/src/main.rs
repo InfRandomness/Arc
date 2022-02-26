54c798b8ba5e1342e3d48f531248fa6468af927e
infrandomness <infrandomness@gmail.com>
2022-02-26 16:04:25 +0100

    Format code

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,9 +1,5 @@
-extern crate core;
-
-use core::panicking::panic;
 use std::env;
 use std::env::Args;
-use std::fmt::format;
 use std::iter::Skip;
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -50,16 +46,22 @@ fn image(mut args: Skip<Args>) -> Result<(), DynError> {
 
 fn create_disk_image(kernel_binary_path: &Path, target_json: String) -> PathBuf {
     let kernel_manifest_path = PathBuf::from("../kernel");
-    let bootloader_manifest_path = bootloader_locator::locate_bootloader("bootloader", Some(&kernel_manifest_path)).unwrap();
+    let bootloader_manifest_path =
+        bootloader_locator::locate_bootloader("bootloader", Some(&kernel_manifest_path)).unwrap();
 
     let mut build_cmd = Command::new(env!("CARGO"));
     build_cmd.current_dir(bootloader_manifest_path.parent().unwrap());
     build_cmd.arg("builder");
-    build_cmd.arg("--kernel-manifest")
+    build_cmd
+        .arg("--kernel-manifest")
         .arg(&kernel_manifest_path);
     build_cmd.arg("--kernel-binary").arg(&kernel_binary_path);
-    build_cmd.arg("--target-dir").arg(kernel_manifest_path.parent().unwrap().join("target"));
-    build_cmd.arg("--out-dir").arg(kernel_binary_path.parent().unwrap());
+    build_cmd
+        .arg("--target-dir")
+        .arg(kernel_manifest_path.parent().unwrap().join("target"));
+    build_cmd
+        .arg("--out-dir")
+        .arg(kernel_binary_path.parent().unwrap());
     build_cmd.arg("--target").arg(target_json);
     build_cmd.arg("--quiet");
 
@@ -68,9 +70,15 @@ fn create_disk_image(kernel_binary_path: &Path, target_json: String) -> PathBuf 
     }
 
     let kernel_binary_name = kernel_binary_path.file_name().unwrap().to_str().unwrap();
-    let disk_image = kernel_binary_path.parent().unwrap().join(format!("boot-bios-{}.img", kernel_binary_name));
+    let disk_image = kernel_binary_path
+        .parent()
+        .unwrap()
+        .join(format!("boot-bios-{}.img", kernel_binary_name));
     if !disk_image.exists() {
-        panic!("Disk image does not exist at {} after bootloader build", disk_image.display());
+        panic!(
+            "Disk image does not exist at {} after bootloader build",
+            disk_image.display()
+        );
     }
     disk_image
 }
